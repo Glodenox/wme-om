5aad9b92daf4cf9e0f597efd0dbd3140c25c0ace
Glodenox <github@tomputtemans.com>
2015-10-24 22:58:36 +0200

    Preparation for other maps Ability to adjust opacity

--- WME_OpenMaps.user.js
+++ WME_OpenMaps.user.js
@@ -5,7 +5,7 @@
 // @include     https://www.waze.com/*/editor/*
 // @include     https://www.waze.com/editor/*
 // @include     https://editor-beta.waze.com/*
-// @version     1.0
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
@@ -19,13 +19,54 @@
 		}
 		log('OM initated');
 
-		var geopunt = new OL.Layer.WMS('Geopunt GRB',
-			'https://grb.agiv.be/geodiensten/raadpleegdiensten/GRB/wms',
-			{ layers: "GRB_Basiskaart" }
-		);
-		Waze.map.addLayer(geopunt);
+		var layers = [
+			// Geopunt: Flanders (Belgium)
+			new OL.Layer.WMS('Geopunt GRB',
+				'https://grb.agiv.be/geodiensten/raadpleegdiensten/GRB/wms',
+				{ layers: "GRB_Basiskaart" }
+			)
+		];
+		Waze.map.addLayers(layers);
+		if (typeof localStorage.OM_opacity == 'undefined') {
+			localStorage.OM_opacity = 100;
+		}
+
+		var opacityDiv = document.createElement('div');
+		opacityDiv.className = 'WazeControlLocationInfo';
+		opacityDiv.style.position = 'absolute';
+		opacityDiv.style.right = '20px';
+		opacityDiv.style.left = 'auto';
+		opacityDiv.style.top = '40px';
+		opacityDiv.style.zIndex = 1020;
+		opacityDiv.style.display = 'none';
+		var opacitySlider = document.createElement('input');
+		opacitySlider.type = 'range';
+		opacitySlider.max = 100;
+		opacitySlider.min = 5;
+		opacitySlider.step = 5;
+		opacitySlider.value = localStorage.OM_opacity;
+		opacitySlider.style.verticalAlign = 'middle';
+		opacitySlider.addEventListener('input', function() {
+			layers.map(function(layer) {
+				layer.setOpacity(opacitySlider.value / 100);
+			});
+			localStorage.OM_opacity = opacitySlider.value;
+		});
+		opacityDiv.appendChild(document.createTextNode('Opacity: '));
+		opacityDiv.appendChild(opacitySlider);
+		document.getElementById('WazeMap').appendChild(opacityDiv);
+		layers.map(function(layer) {
+			layer.events.register('visibilitychanged', null, function(e) {
+				opacityDiv.style.display = (layer.visibility ? 'block' : 'none');
+			});
+			layer.setOpacity(localStorage.OM_opacity / 100);
+		});
+
+		// Necessary as the layer doesn't update when a zoom has occurred
 		Waze.map.events.register('zoomend', null, function() {
-			geopunt.redraw();
+			layers.map(function(layer) {
+				layer.redraw();
+			});
 		});
 	}
 
